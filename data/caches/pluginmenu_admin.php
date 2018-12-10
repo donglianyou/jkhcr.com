25c7b514194769dea4c69e7d423b6cc6b12cfdef
donglianyou <158624818@qq.com>
2018-12-10 17:49:19 +0800

    first commit

--- data/caches/pluginmenu_admin.php
+++ data/caches/pluginmenu_admin.php
@@ -1,6 +1,6 @@
 <?php
 $data = array (
-  /*'优惠券' => 
+  '优惠券' => 
   array (
     '优惠券分类' => 'coupon_category.php',
     '已上传优惠券' => 'coupon_list.php',
@@ -16,7 +16,7 @@ $data = array (
     '新闻管理' => 'news.php',
     '新闻类别' => 'channel.php',
     '新闻评论' => 'news_comment.php',
-  ),*/
+  ),
   '商品' => 
   array (
     '商品分类' => 'goods_category.php',
