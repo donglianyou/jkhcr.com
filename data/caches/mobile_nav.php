00635196094262d2e0a38b804c67d018773b09a8
donglainyou <1158624818@qq.com>
2019-01-16 16:40:37 +0800

    first commit

--- data/caches/mobile_nav.php
+++ data/caches/mobile_nav.php
@@ -13,8 +13,8 @@ $data = array (
     ),
     1 => 
     array (
-      'title' => '信息分类',
-      'url' => 'index.php?mod=category',
+      'title' => '新闻资讯',
+      'url' => 'index.php?mod=news',
       'color' => '',
       'flag' => 'category',
       'ico' => '',
@@ -22,10 +22,19 @@ $data = array (
     ),
     2 => 
     array (
-      'title' => '热点资讯',
-      'url' => 'index.php?mod=news',
+      'title' => '燕窝及高端滋补品',
+      'url' => 'index.php?mod=corporation',
       'color' => '',
       'flag' => 'news',
+      'ico' => '',
+      'target' => '_self',
+    ),
+    56 => 
+    array (
+      'title' => '协会专栏',
+      'url' => 'index.php?mod=xiehui',
+      'color' => '',
+      'flag' => 'xiehui',
       'ico' => '',
       'target' => '_self',
     ),
@@ -38,12 +47,30 @@ $data = array (
       'ico' => '',
       'target' => '_self',
     ),
+    57 => 
+    array (
+      'title' => '品牌推荐',
+      'url' => 'index.php?mod=brand',
+      'color' => '',
+      'flag' => 'brand',
+      'ico' => '',
+      'target' => '_self',
+    ),
     55 => 
     array (
-      'title' => '商品展示',
+      'title' => '在线商城',
       'url' => 'index.php?mod=goods',
       'color' => '',
       'flag' => 'goods',
+      'ico' => '',
+      'target' => '_self',
+    ),
+    58 => 
+    array (
+      'title' => '展会',
+      'url' => 'index.php?mod=zhanhui',
+      'color' => '',
+      'flag' => 'zhanhui',
       'ico' => '',
       'target' => '_self',
     ),
